d421e941210e951ce62362623603bb8d8dceb1d2
Sergey Maltsev <smaltsev@telematika.com>
2023-05-06 22:13:31 +0300

    add multiple graphs

--- tests/instance_test.go
+++ tests/instance_test.go
@@ -2,23 +2,36 @@ package tests
 
 import (
 	"github.com/kkkooottt/go-graph-service"
-	"strconv"
 	"testing"
 )
 
 func Test_Instance(t *testing.T) {
+
 	t.Log("start")
+
 	grph := graph.New("8080")
+
 	t.Log("new instance inited")
-	var chart []graph.ChartPoint
-	points := []float64{1, 3, 4.5, 5, 10}
-	for k, v := range points {
-		ch := graph.ChartPoint{
-			Label: strconv.Itoa(k),
-			Value: v,
-		}
-		chart = append(chart, ch)
+
+	var chart graph.Reply
+
+	chart.Type = graph.TypeLine // "bar" "line"
+
+	app := graph.Datasets{
+		Data:        []float64{1, 3, 4.5, 5, 10},
+		Label:       "test",
+		BorderWidth: 2,
 	}
+
+	app2 := graph.Datasets{
+		Data:        []float64{10, 5, 3, 2, 1},
+		Label:       "test2",
+		BorderWidth: 2,
+	}
+	chart.Data.Datasets = append(chart.Data.Datasets, app)
+	chart.Data.Datasets = append(chart.Data.Datasets, app2)
+	chart.Data.Labels = []string{"1", "2", "3", "4", "5"}
+
 	t.Log("test data prepared")
 
 	grph.Start()
